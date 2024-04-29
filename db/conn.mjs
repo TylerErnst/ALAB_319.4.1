5e5faf581f21baef9714bf08e14004c900d80336
TylerErnst <tcernst5@gmail.com>
2024-04-29 15:49:40 -0600

    Most of part 2

---
+++ db/conn.mjs
@@ -0,0 +1,17 @@
+// import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString);
+
+let conn;
+try{
+    conn = await client.connect();
+    console.log('Connected to MongoDB');
+} catch(err) {
+    console.log(err)
+}
+
+let db = conn.db("sample_training");
+
+export default db;
