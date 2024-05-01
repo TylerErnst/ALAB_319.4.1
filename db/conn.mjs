1bb376096112f08c72a79888caa9a30f31382758
TylerErnst <tcernst5@gmail.com>
2024-05-01 14:49:52 -0600

    Validation

--- db/conn.mjs
+++ db/conn.mjs
@@ -14,4 +14,18 @@ try{
 
 let db = conn.db("sample_training");
 
+// Create indexes
+(async () => {
+    const collection = await db.collection("grades");
+    
+    // Create a single-field index on class_id.
+    await collection.createIndex({ class_id: 1 });
+  
+    // Create a single-field index on learner_id.
+    await collection.createIndex({ learner_id: 1 });
+  
+    // Create a compound index on learner_id and class_id, in that order, both ascending.
+    await collection.createIndex({ learner_id: 1, class_id: 1 });
+  })();
+
 export default db;
--- index.mjs
+++ index.mjs
@@ -8,19 +8,29 @@ const app = express();
 
 app.use(express.json());
 
-// Create indexes
-(async () => {
-  const collection = await db.collection("grades");
-  
-  // Create a single-field index on class_id.
-  await collection.createIndex({ class_id: 1 });
-
-  // Create a single-field index on learner_id.
-  await collection.createIndex({ learner_id: 1 });
-
-  // Create a compound index on learner_id and class_id, in that order, both ascending.
-  await collection.createIndex({ learner_id: 1, class_id: 1 });
-})();
+//Validation
+db.collection('grades', {
+	validator: {
+		$jsonSchema: {
+			bsonType: 'object',
+			required: ['class_id', 'learner_id'],
+			properties: {
+				class_id: {
+					bsonType: 'int',
+					minimum: 0,
+					maximum: 300,
+					description: 'class_id must be an integer between 0 and 300'
+				},
+				learner_id: {
+					bsonType: 'int',
+					minimum: 0,
+					description: 'learner_id must be an integer greater than or equal to 0'
+				}
+			}
+		}
+	},
+	validationAction: 'warn'
+});
 
 
 // The schema
